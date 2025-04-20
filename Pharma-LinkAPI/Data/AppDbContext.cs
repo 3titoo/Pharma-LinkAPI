0038f26bada2eb3b569179435789f29d18d4f7f3
3titoo <155198383+3titoo@users.noreply.github.com>
2025-04-20 17:47:35 +0200

    Add project files.

---
+++ Pharma-LinkAPI/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Pharma_LinkAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+
+        
+    }
+}
---
+++ Pharma-LinkAPI/Repositries/Irepo.cs
@@ -0,0 +1,11 @@
+﻿namespace Pharma_LinkAPI.Repositries
+{
+    public interface Irepo<T>
+    {
+        Task<T> GetById(int id);
+        Task<IEnumerable<T>> GetAll();
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(int id);
+    }
+}
