77b52b57a73e52836b87586ae04da5bf0064bc47
3titoo <155198383+3titoo@users.noreply.github.com>
2025-05-03 05:24:58 +0300

    ..

--- Pharma-LinkAPI/Repositries/Repositry/RequestRepo.cs
+++ Pharma-LinkAPI/Repositries/Repositry/RequestRepo.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.BlazorIdentity.Pages.Manage;
 using Pharma_LinkAPI.Data;
 using Pharma_LinkAPI.Models;
 using Pharma_LinkAPI.Repositries.Irepositry;
@@ -40,6 +41,16 @@ namespace Pharma_LinkAPI.Repositries.Repositry
         {
             var request = _db.Requests.FirstOrDefault(i => i.Id == id);
             return request;
+        }
+
+        public async Task<Request?> GetUserByEmail(string email)
+        {
+            return await _db.Requests.FirstOrDefaultAsync(i => i.Email == email);
+        }
+
+        public async Task<Request?> GetUserByusername(string username)
+        {
+            return await _db.Requests.FirstOrDefaultAsync(i => i.UserName == username);
         }
 
         public void Update(Request entity)
--- Pharma-LinkAPI/Services/Attributes/IsUserNameInUseAttribute .cs
+++ Pharma-LinkAPI/Services/Attributes/IsUserNameInUseAttribute .cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Pharma_LinkAPI.Identity;
+using Pharma_LinkAPI.Repositries.Irepositry;
 using System.ComponentModel.DataAnnotations;
 
 namespace Pharma_LinkAPI.Services.Attributes
@@ -13,8 +14,10 @@ namespace Pharma_LinkAPI.Services.Attributes
             {
                 string username = (string)value;
                 var userManager = (UserManager<AppUser>)validationContext.GetService(typeof(UserManager<AppUser>));
+                var x = (IrequestRepositry)validationContext.GetService(typeof(IrequestRepositry));
+                var request = x.GetUserByusername(username);
                 var user = userManager.FindByNameAsync(username).Result;
-                if (user == null)
+                if (user == null && request == null)
                 {
                     return ValidationResult.Success;
                 }
